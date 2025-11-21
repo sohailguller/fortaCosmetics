abfaa7743afe5128f956e16e0072bc931898d987
Imran Guller <ims03g@gmail.com>
2025-11-21 04:59:07 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -185,15 +185,15 @@ export default function Home() {
         </section>
 
         {/* Editorial Section 1 - Split */}
-        <section className="grid md:grid-cols-2 bg-[#F5F5F5]">
-          <div className="relative aspect-square md:aspect-auto min-h-[500px]">
+        <section className="grid md:grid-cols-2 bg-[#F5F5F5] w-full overflow-hidden">
+          <div className="relative aspect-square md:aspect-auto min-h-[500px] w-full">
             <img 
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68fae7032e9ee5cc70e1bfa7/ddbbe1ce2_TheVaultStock-10296.jpg"
               alt="Editorial"
               className="absolute inset-0 w-full h-full object-cover"
             />
           </div>
-          <div className="flex flex-col justify-center p-12 md:p-20 bg-[#EBEBEB]">
+          <div className="flex flex-col justify-center p-6 md:p-20 bg-[#EBEBEB] w-full">
             <h2 className="text-3xl md:text-4xl font-bold mb-6 uppercase leading-tight">
               Makeup that's<br/>good for your<br/>sweat.
             </h2>
