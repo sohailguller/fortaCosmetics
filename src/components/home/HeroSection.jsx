212c01aa3bf41b13d3ab43775c9d70abf5b9f257
Imran Guller <ims03g@gmail.com>
2026-01-02 01:41:58 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,18 +1,9 @@
-import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React from "react";
 
 export default function HeroSection() {
-  const ref = useRef(null);
-  const { scrollY } = useScroll();
-  // Parallax: Image moves slower than scroll (0 to 400px down as we scroll 0 to 1000px)
-  const y = useTransform(scrollY, [0, 1000], [0, 400]);
-
   return (
-    <section ref={ref} className="relative h-screen w-full overflow-hidden bg-[#F5F5F5]">
-      <motion.div 
-        style={{ y }}
-        className="absolute inset-0 flex"
-      >
+    <section className="relative h-screen w-full overflow-hidden bg-[#F5F5F5]">
+      <div className="absolute inset-0 flex">
         <img
           src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68fae7032e9ee5cc70e1bfa7/542f0db57_2-05636.jpg"
           alt="Hero Left"
@@ -28,11 +19,11 @@ export default function HeroSection() {
           alt="Hero Right"
           className="w-1/3 h-full object-cover"
         />
-      </motion.div>
+      </div>
       
       <div className="absolute inset-0 bg-black/10" />
       
-      <div className="absolute bottom-16 left-6 md:bottom-20 md:left-12 flex flex-col items-start text-left">
+      <div className="absolute bottom-20 left-6 md:bottom-24 md:left-12 flex flex-col items-start text-left">
         <h1 className="text-white font-serif text-xl md:text-3xl font-normal tracking-tight mb-4 max-w-xl leading-[1.2]">
           You don't have to sit still to look pretty.
         </h1>
