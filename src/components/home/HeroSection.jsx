d373ea6042430e6646642b171f518aaf30e81aff
Imran Guller <ims03g@gmail.com>
2026-01-07 06:20:27 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <section className="relative h-[70vh] md:h-screen w-full overflow-hidden bg-[#F5F5F5]">
-      <div className="fixed top-0 left-0 right-0 h-[70vh] md:h-screen flex -z-10">
+    <section className="relative h-[70vh] md:h-screen w-full overflow-hidden">
+      <div className="fixed top-0 left-0 right-0 h-[70vh] md:h-screen flex z-0">
         <img
           src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68fae7032e9ee5cc70e1bfa7/542f0db57_2-05636.jpg"
           alt="Hero Left"
@@ -21,9 +21,9 @@ export default function HeroSection() {
         />
       </div>
       
-      <div className="fixed top-0 left-0 right-0 h-[70vh] md:h-screen bg-gradient-to-t from-black/50 via-black/10 to-transparent md:bg-black/10 -z-10" />
+      <div className="fixed top-0 left-0 right-0 h-[70vh] md:h-screen bg-gradient-to-t from-black/50 via-black/10 to-transparent md:bg-black/10 z-0" />
       
-      <div className="absolute bottom-20 left-6 md:bottom-24 md:left-12 flex flex-col items-start text-left">
+      <div className="absolute bottom-20 left-6 md:bottom-24 md:left-12 flex flex-col items-start text-left z-10">
         <h1 className="text-white font-serif text-xl md:text-3xl font-normal tracking-tight mb-4 max-w-xl leading-[1.2] drop-shadow-lg md:drop-shadow-none">
           You don't have to sit still to look pretty.
         </h1>
