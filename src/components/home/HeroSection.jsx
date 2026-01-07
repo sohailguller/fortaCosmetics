e2de9af066ae57da2f227b675924563b9d2a5e33
Imran Guller <ims03g@gmail.com>
2026-01-07 06:43:15 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
       
       <div className="fixed top-0 left-0 right-0 h-[70vh] md:h-screen bg-gradient-to-t from-black/50 via-black/10 to-transparent md:bg-black/10 z-0" />
       
-      <div className="absolute bottom-20 left-6 md:bottom-24 md:left-12 flex flex-col items-start text-left z-10">
+      <div className="absolute bottom-20 left-8 md:bottom-24 md:left-16 flex flex-col items-start text-left z-10">
         <h1 className="text-white font-serif text-xl md:text-3xl font-normal tracking-tight mb-4 max-w-xl leading-[1.2] drop-shadow-lg md:drop-shadow-none">
           You don't have to sit still to look pretty.
         </h1>
